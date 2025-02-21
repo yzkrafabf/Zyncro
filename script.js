15e5a8d9e55b7b32d9ae63e08f7b9b3545ecf8a6
yzkrafabf <yzkrafabf@gmail.com>
2025-02-21 10:09:04 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".titulo").forEach(titulo => {
-    titulo.addEventListener("mouseover", () => {
-        titulo.nextElementSibling.style.display = "block";
-    });
-
-    titulo.addEventListener("mouseout", () => {
-        titulo.nextElementSibling.style.display = "none";
-    });
-});
