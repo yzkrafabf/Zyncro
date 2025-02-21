1417dfbbd2d79f6d808b4ca404894980724c65dd
yzkrafabf <yzkrafabf@gmail.com>
2025-02-20 18:45:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function mostrarEjemplo() {
+    const codigoEjemplo = `
+# Código de ejemplo en Python
+print("Hola, mundo!")
+    `;
+    document.getElementById("ejemploCodigo").textContent = codigoEjemplo;
+}
