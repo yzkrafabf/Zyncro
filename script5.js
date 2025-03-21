2182d8e1e4822c86c8cff9f4572339e6cfe9ee41
yzkrafabf <yzkrafabf@gmail.com>
2025-03-20 21:54:20 -0600

    Delete script5.js

--- script5.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("togglePassword").addEventListener("change", function() {
-    let passwordField = document.getElementById("password");
-    passwordField.type = this.checked ? "text" : "password";
-});
